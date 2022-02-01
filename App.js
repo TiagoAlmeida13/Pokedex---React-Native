e6ce5eeee67ee961ed95edfb488177e182e435a6
TiagoAlmeidaLikeWaterCS <tiago.almeida@likewatercs.com>
2022-02-01 18:54:50 -0300

    Pokedex feita em React Native utilizando EXPO.

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import Pokemons from './Components/Pokemons';
+import Details from './Components/Details';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const appNavigator = createStackNavigator(
+  {
+    Home: {
+      screen: Pokemons,
+    },
+    Details: {
+      screen: Details,
+    },
+  },
+  {
+    initialRouteName: 'Home',
+  },
+);
+
+const AppContainer = createAppContainer(appNavigator);
+
+class App extends Component {
+  render() {
+    return <AppContainer />;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
